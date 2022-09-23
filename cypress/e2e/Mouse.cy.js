a9753ac366c6755a4915b1578978c5670f1fb38c
proffatai <proffatai@gmail.com>
2022-09-23 06:38:13 +0100

    Updated

--- cypress/e2e/Mouse.cy.js
+++ cypress/e2e/Mouse.cy.js
@@ -23,13 +23,23 @@ describe('Mouse Actions',() => {
     cy.get('#nav-flyout-accountList').should('not.be.visible')
     cy.contains("Your Account").should('not.be.visible')
     cy.get('#nav-link-accountList').trigger('mouseover')
-    cy.get('#nav-flyout-accountList').should('be.visible')
+    cy.get('#nav-flyout-accountLi,t').should('be.visible')
     cy.contains("Your Account").should('be.visible')
   })
 
   it.only('Mouse over Event 2',{pageLoadTimeout:90000}, () => { 
     cy.visit('http://www.automationpractice.com') // this fucking website takes a lot of time to load so added extra page timeout
-    cy.get('.sf-menu > :nth-child(1) > [href="http://automationpractice.com/index.php?id_category=3&controller=category"]').trigger('mouseover') //The sub category isnt appearing
-    cy.contains('Tops').invoke('show')
+     //The sub category isnt appearing despite being hovered on 
+    cy.get('.sf-menu > :nth-child(1) > [href="http://automationpractice.com/index.php?id_category=3&controller=category"]').trigger('mouseover')
+    //class "submenu-container clearfix first-in-line-xs" exist twice on the platform so we can use the first, last or eq() to specify
+    // the exact class that we want. I had to modify the class name by removing all the spaces present in between some of the words
+    //with . before performing the get request
+    cy.get('.submenu-container.clearfix.first-in-line-xs').first().invoke('show')//we wanted to ensure that the subcategory elements
+    //gets displayed after being hovered on, so we had to get(locator of the component inside the hovered section), 
+// in this case, there is an ul with a class name "submenu-container clearfix first-in-line-xs" inside the Women menu that houses both
+// Tops and Dresses, so we access that class, after we have hovered on Women, then since the same class exist twice, we used first()
+// to get the exact one we need, and we used the invoke('show') to forcely display the elements after being hovered on
+
+cy.contains('Tops').should('be.visible') // we want to assert that Tops is visible after the hidden elements under Women is hovered on
   })
 })
